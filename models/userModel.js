72af96293dafe2673d87ad1918aed94aa767e553
maneeshakaravita99 <maneeshakaravita@gmail.com>
2022-10-05 15:10:05 +0530

    upadted

---
+++ models/userModel.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+      min: 6,
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/avanoa/image/upload/v1649475938/avatar/default_avatar_oyb8ep.png",
+    },
+  },
+  { timestamp: true }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,42 @@
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require('cors');
+const app = express();
+require("dotenv").config();
+
+//Import Routes
+const reminderRoutes =require('./Routes/reminderRoutes');
+const userRoutes = require("./routes/userRoutes");
+const uploadRoutes = require("./routes/uploadRoutes");
+
+const port = process.env.PORT || 8000
+// db
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("db connected");
+    app.listen(port, () => {
+      console.log("server is active");
+    });
+  }
+);
+
+// mw
+app.use(express.json());
+express.urlencoded({ extended: true });
+app.use(cookieParser());
+app.use("/uploads", express.static("uploads"));
+app.use(cors({ origin: true, credentials: true }));
+
+// routes
+app.use(userRoutes);
+app.use(uploadRoutes);
+app.use(reminderRoutes);
